36ca54effb7412a5300d8b407791d5c62530cefa
Wafa009 <naziyatwafa@gmail.com>
2026-03-16 21:11:30 +0600

    Adding CRUD in manager.py

--- manager.py
+++ manager.py
@@ -13,3 +13,27 @@ class TaskManager:
  
     def save(self):
         self.storage.save([t.to_dict() for t in self.tasks])
+
+
+
+
+# -------- CRUD --------
+ 
+    def add_task(self, task):
+        self.tasks.append(task)
+        self.save()
+ 
+    def delete_task(self, index):
+        if 0 <= index < len(self.tasks):
+            del self.tasks[index]
+            self.save()
+ 
+    def update_task(self, index, new_task):
+        if 0 <= index < len(self.tasks):
+            self.tasks[index] = new_task
+            self.save()
+ 
+    def toggle_complete(self, index):
+        if 0 <= index < len(self.tasks):
+            self.tasks[index].completed = not self.tasks[index].completed
+            self.save()
